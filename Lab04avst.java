6da92d6688ff3907d9f3e565d407b40839f5eb25
14276 <analiese.drupka@vtsd.com>
2022-09-29 10:07:13 -0400

    t

--- Lab04avst.java
+++ Lab04avst.java
@@ -12,6 +12,13 @@ public class Lab04avst
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
+        double monthlyRate = (4.85/12);
+        double monthlyPaid = (12+ numYears);
+        double monthlyPayment = (monthlyRate * (Math.pow(monthlyRate + 1, monthlyPaid))/(Math.pow) );
+
+        System.out.println("principal: $" +principal);
+        System.out.println("anaualRate: "    + annualRate +  "%");
+        System.out.println("numYears: " +numYears);
 
 
 
